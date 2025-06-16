121c4c02539584ed490c9fc17a2355ef0c1f1cd0
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-16 17:13:43 +0600

    Add file upload success message to Data Analysis App

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 st.title("Data Analysis App")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
