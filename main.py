2b0f7c5c4faf963aa07219d987e505a40b2d7266
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-16 17:20:00 +0600

    Implement data preview, summary, and filtering features in Data Analysis App

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ st.title("Data Analysis App")
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
-    st.write("File uploaded successfully!")
+    df= pd.read_csv(uploaded_file)
+    
+    st.write("Data Preview:")
+    st.write(df.head())
+    
+    st.write("Data Summary:")
+    st.write(df.describe())
+    
+    st.subheader("Filter Data ")
+    column_to_filter = st.selectbox("Select column to filter", df.columns)
+    filter_value = st.text_input("Enter value to filter")
+    filtered_data = df[df[column_to_filter] == filter_value]
+    st.write("Filtered Data:")
+    st.write(filtered_data)
